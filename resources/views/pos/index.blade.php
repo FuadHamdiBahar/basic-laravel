a59c5644d6917ce5349149982ceb685de34e5d0a
Fuad Hamdi Bahar <fuadhamdi9(@gmail.com>
2025-04-13 00:50:03 +0700

    fix minor

--- resources/views/pos/index.blade.php
+++ resources/views/pos/index.blade.php
@@ -5,7 +5,7 @@
         <div class="col-xl-12">
             <div class="card shadow mb-4">
                 <div class="card-header py-3">
-                    <h6 class="m-0 font-weight-bold text-primary">Basic Card Example</h6>
+                    <h6 class="m-0 font-weight-bold text-primary">POS</h6>
                 </div>
                 <div class="card-body">
                     <form action="/api/transaction" method="post"  id="addTransaction" name="addTransaction">
