01d09b88764e2e72028258aa980a7b63cb544121
Fuad Hamdi Bahar <fuadhamdi9(@gmail.com>
2025-04-12 20:35:44 +0700

    done

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ViewController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [ViewController::class, 'index']);
+Route::get('/pos', [ViewController::class, 'posIndex'])->name('pos');
+Route::get('/transaction', [ViewController::class, 'listTransaction'])->name('listTransaction');
+
+Route::post('/transaction', [TransactionController::class, 'createTransaction']);
